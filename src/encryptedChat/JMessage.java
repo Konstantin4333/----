bef1f0344976ec1c045add64985ca9c784ac80a4
Konstantin <konstantin.m.d13@gmai.com>
2023-05-16 23:46:33 +0300

    InitialCommit

---
+++ src/encryptedChat/JMessage.java
@@ -0,0 +1,11 @@
+package encryptedChat;
+import java.io.Serializable;
+
+public class JMessage implements Serializable {
+
+    String text;
+    public JMessage(String input) {
+        text = input;
+    }
+
+}
